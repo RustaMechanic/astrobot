bfab1e3248c61ebe6d34386f82a063dc47ea7421
rustamechanic <rustam@idrisov.me>
2024-12-07 19:05:54 +0800

    Implemented game loop in the main() function with a black updating screen.

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import pygame
+from constants import *
 
 def main():
-    print("Starting asteroids!")
+    pygame.init()
+
+    while True:
+        screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        screen.fill((0,0,0))
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        pygame.display.flip()
+
+
 
 if __name__ == "__main__":
     main()
